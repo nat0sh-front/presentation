08743d54e26aa599845a8bce50a6208840aa8eb7
natali <gvozd.n.y@yandex.kz>
2025-09-26 20:46:15 +0500

    slide02 fix popup animation

--- scripts/common.js
+++ scripts/common.js
@@ -1,37 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const infoButton = document.querySelector('.button__info');
-  const popup = document.querySelector('#popup-info');
-  const popupOverlay = popup.querySelector('.popup__overlay');
-  const popupBody = popup.querySelector('.popup__body');
-  const closeButton = popup.querySelector('.popup__close');
+const infoButton = document.querySelector('.button__info');
+const popup = document.querySelector('#popup-info');
+const popupOverlay = popup.querySelector('.popup__overlay');
+const popupBody = popup.querySelector('.popup__body');
+const popupText = popup.querySelector('.popup__text');
+const closeButton = popup.querySelector('.popup__close');
 
-  infoButton.addEventListener('click', () => {
+infoButton.addEventListener('click', () => {
     popup.classList.add('active');
+    popupOverlay.style.animation = "fadeIn 0.2s forwards";
+    popupBody.style.animation = "popupShow 0.5s forwards";
+    popupText.style.animation = "fadeIn 0.6s forwards 0.5s";
     infoButton.style.opacity = "0.6";
     infoButton.disabled = true;
-  });
+})
 
-  const closePopup = () => {
-    popup.classList.remove('active');
+closeButton.addEventListener('click', () => {
+    popupOverlay.style.animation = "fadeOut 0.3s forwards";
+    popupBody.style.animation = "popupHide 0.5s forwards";
+    popupText.style.animation = "fadeOut 0.3s forwards";
+    infoButton.style.opacity = "1";
+    infoButton.disabled = false;
+})
 
-    const onTransitionEnd = (e) => {
-      if (e.propertyName === 'transform' && e.target === popupBody) {
-        infoButton.style.opacity = "1";
-        infoButton.disabled = false;
-        popupBody.removeEventListener('transitionend', onTransitionEnd);
-      }
-    };
-
-    popupBody.addEventListener('transitionend', onTransitionEnd);
-
-    setTimeout(() => {
-      if (infoButton.disabled) {
-        infoButton.style.opacity = "1";
-        infoButton.disabled = false;
-        popupBody.removeEventListener('transitionend', onTransitionEnd);
-      }
-    }, 700);
-  };
-
-  closeButton.addEventListener('click', closePopup);
+popupBody.addEventListener("animationend", (e) => {
+  if (e.animationName === "popupHide") {
+    popup.classList.remove("active");
+    popupOverlay.style.animation = "";
+    popupBody.style.animation = "";
+    popupText.style.animation = "";
+  }
 });
