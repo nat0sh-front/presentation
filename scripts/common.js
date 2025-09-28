8ec3a902093580e55916d4bfeab4497a29149dfc
natali <gvozd.n.y@yandex.kz>
2025-09-28 15:47:28 +0500

    prettier format

--- scripts/common.js
+++ scripts/common.js
@@ -1,26 +1,26 @@
-const infoButton = document.querySelector('.button__info');
-const popup = document.querySelector('#popup-info');
-const popupOverlay = popup.querySelector('.popup__overlay');
-const popupBody = popup.querySelector('.popup__body');
-const popupText = popup.querySelector('.popup__text');
-const closeButton = popup.querySelector('.popup__close');
+const infoButton = document.querySelector(".button__info");
+const popup = document.querySelector("#popup-info");
+const popupOverlay = popup.querySelector(".popup__overlay");
+const popupBody = popup.querySelector(".popup__body");
+const popupText = popup.querySelector(".popup__text");
+const closeButton = popup.querySelector(".popup__close");
 
-infoButton.addEventListener('click', () => {
-    popup.classList.add('active');
-    popupOverlay.style.animation = "fadeIn 0.2s forwards";
-    popupBody.style.animation = "popupShow 0.5s forwards";
-    popupText.style.animation = "fadeIn 0.6s forwards 0.5s";
-    infoButton.style.opacity = "0.6";
-    infoButton.disabled = true;
-})
+infoButton.addEventListener("click", () => {
+  popup.classList.add("active");
+  popupOverlay.style.animation = "fadeIn 0.2s forwards";
+  popupBody.style.animation = "popupShow 0.5s forwards";
+  popupText.style.animation = "fadeIn 0.6s forwards 0.5s";
+  infoButton.style.opacity = "0.6";
+  infoButton.disabled = true;
+});
 
-closeButton.addEventListener('click', () => {
-    popupOverlay.style.animation = "fadeOut 0.3s forwards";
-    popupBody.style.animation = "popupHide 0.5s forwards";
-    popupText.style.animation = "fadeOut 0.3s forwards";
-    infoButton.style.opacity = "1";
-    infoButton.disabled = false;
-})
+closeButton.addEventListener("click", () => {
+  popupOverlay.style.animation = "fadeOut 0.3s forwards";
+  popupBody.style.animation = "popupHide 0.5s forwards";
+  popupText.style.animation = "fadeOut 0.3s forwards";
+  infoButton.style.opacity = "1";
+  infoButton.disabled = false;
+});
 
 popupBody.addEventListener("animationend", (e) => {
   if (e.animationName === "popupHide") {
